fc3605bdb01df8eaa507ad2d1625cab982b3d00a
EtnoPolino <einshoch66@gmail.com>
2022-04-10 00:50:08 +0200

    sketch of what i want to achieve

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+    notre div "notecontainer" est en diplay : flexwrap pour rentrer la largeur 
+    
+    j'appuie sur "add Note" (si il n'ya rien dans la note, il ne se passe rien)
+      - je crée une div et je lui attribue la class "divnote" contenant : 
+        - un titre en h4 : Note 1, 2, ou 3.. selon le nombre de note [à créer et append à divnote]
+        - un paragraphe ou y'aura notre notre de textarea [à créer et append à divnote]
+        - un bouton 'View Detail' [à créer et append à divnote ]
+
+            ==> J'appuis sur le boutton 'View Detail' et il apparait
+                - une div appele 'modal' contenant : 
+                    - paragraphe avec tout mon texte en note [à créer et append à modal]
+                    - un bouton close avec un id "close" disparition du modal [à créer et append à modal]
+                 
+
+
+La première chose à faire est de créer les classes CSS et tester un html de :
+    - notecontainer
+    - divnote   
+    - modal
+*/

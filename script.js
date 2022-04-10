b57cb58677ff5c83f659165de073d90ddcb841c1
EtnoPolino <einshoch66@gmail.com>
2022-04-10 05:37:35 +0200

    add note, note counter not done yet

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
             ==> J'appuis sur le boutton 'View Detail' et il apparait
                 - une div appele 'modal' contenant : 
                     - paragraphe avec tout mon texte en note [à créer et append à modal]
-                    - un bouton close avec un id "close" disparition du modal [à créer et append à modal]
+                    - un bouton close avec une class "close" disparition du modal [à créer et append à modal]
                  
 
 
@@ -19,3 +19,42 @@ La première chose à faire est de créer les classes CSS et tester un html de :
     - divnote   
     - modal
 */
+
+const buttonAddNote = document.querySelector("#submit");
+const noteContainer = document.querySelector('#notecontainer');
+const textArea = document.querySelector('#note');
+
+function addNote(e){
+
+    if(!textArea.value) return;
+
+/* creation of the note */
+    const divNote = document.createElement('div');
+    divNote.classList.add('divnote');
+    noteContainer.appendChild(divNote);
+
+/* counter --------- PROBLÈME SUR LE COMPTAGE DES PAGES */
+let count = 1;
+
+/* remplissage de notre divnote */
+    const numberNote = document.createElement('h4');
+    const paragraphDiv = document.createElement('p');
+    const buttonDetails = document.createElement('button');
+
+    numberNote.innerText = `Note ${count++}`;
+    paragraphDiv.innerText = textArea.value;
+    buttonDetails.innerText = 'View Detail';
+
+    numberNote.classList.add('h4');
+    paragraphDiv.classList.add('paragraph-div');
+    buttonDetails.classList.add('view-detail');
+
+    divNote.appendChild(numberNote);
+    divNote.appendChild(paragraphDiv);
+    divNote.appendChild(buttonDetails);
+
+
+
+}
+
+buttonAddNote.addEventListener('click', addNote);
